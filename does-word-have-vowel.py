1bb97caa8e18e14d9642760fad0f6cc81adfb51d
Dylan <johnston94@gmail.com>
2018-04-16 17:01:35 -0500

    Count vowels wip

--- does-word-have-vowel.py
+++ does-word-have-vowel.py
@@ -2,27 +2,36 @@ def main():
     alphabet = []
     alphabet = buildAlphabet(alphabet)
     vowels = ['a','e','i','o','u']
+    vowelCount = 0
 
     print "Does this word have a vowel?"
     thisWord = raw_input("enter a word: ")
     
-    if containsVowel(thisWord,vowels):
-        print thisWord, 'has a vowel.'
-    elif containsY(thisWord):
-        print thisWord, "has a vowel, but it's a 'y'"
-    else: 
-        print thisWord, 'does not have a vowel.'
+    if thisWordHasVowel(thisWord,vowels):
+        vowelCount = countVowels(thisWord,vowels,vowelCount)
+        print thisWord, 'has', vowelCount, 'vowels'
+    elif thisWordContainsY(thisWord):
+        vowelCount = countVowels(thisWord,vowels,vowelCount)
+        print thisWord, "has", vowelCount, ", but they are all 'y'"
+    else:
+        print thisWord, 'does not have any vowels.'
 
 def buildAlphabet(alphabet):
     for letter in range(97,122):
         alphabet.append(chr(letter))
 
-def containsVowel(thisWord,vowels):
+def thisWordHasVowel(thisWord,vowels):
     for vowel in vowels: 
         if vowel in thisWord:
-            return True
+            return 1
 
-def containsY(thisWord):
+def countVowels(thisWord,vowels,vowelCount):
+    for vowel in vowels:
+        if vowel in thisWord:
+            vowelCount += 1
+    return vowelCount
+
+def thisWordContainsY(thisWord):
     if 'y' in thisWord:
             return True
 
