69a085957f383957ea3dbc8b401bd913dcbee0f6
Dylan <johnston94@gmail.com>
2018-04-16 16:40:38 -0500

    divide into functions and allow user input

--- does-word-have-vowel.py
+++ does-word-have-vowel.py
@@ -1,18 +1,29 @@
-alphabet = []
-for letter in range(97,122):
-    alphabet.append(chr(letter))
+def main():
+    alphabet = []
+    alphabet = buildAlphabet(alphabet)
+    vowels = ['a','e','i','o','u']
 
-vowels = ['a','e','i','o','u']
-words  = ['apple','banana','when','fgds']
+    print "Does this word have a vowel?"
+    thisWord = raw_input("enter a word: ")
+    
+    if containsVowel(thisWord,vowels):
+        print thisWord, 'has a vowel.'
+    elif containsY(thisWord):
+        print thisWord, "has a vowel, but it's a 'y'"
+    else: 
+        print thisWord, 'does not have a vowel.'
 
-thisWord = words[1] # "banana"
+def buildAlphabet(alphabet):
+    for letter in range(97,122):
+        alphabet.append(chr(letter))
 
 def containsVowel(thisWord,vowels):
     for vowel in vowels: 
         if vowel in thisWord:
             return True
 
-if containsVowel(thisWord,vowels):
-    print thisWord, 'has a vowel.'
-else:
-    print thisWord, 'does not have a vowel.'
+def containsY(thisWord):
+    if 'y' in thisWord:
+            return True
+
+main()
