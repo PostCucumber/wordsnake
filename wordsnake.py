34dc2f60730bacb9372705ad40a47aa54b5eb8b7
postCucumber <johnston94@gmail.com>
2019-07-15 00:24:17 -0500

    wip

--- wordsnake.py
+++ wordsnake.py
@@ -8,14 +8,16 @@ def main():
 
     print "Does this word have a vowel?"
     thisWord = raw_input("enter a word: ")
+    
     characterCount = len(thisWord)
-    vowelCount,consonantCount = countVowels(thisWord,characterCount,vowels,vowelCount)
+    print(characterCount)
+    vowelCount,consonantCount = countVowels(thisWord,characterCount,vowels,vowelCount,consonantCount)
 
 def buildAlphabet(alphabet):
     for letter in range(97,122):
         alphabet.append (chr(letter))
 
-def countVowels(thisWord,characterCount,vowels,vowelCount):
+def countVowels(thisWord,characterCount,vowels,vowelCount,consonantCount):
     for i in range(0,characterCount):
         if thisWord[i] == vowels[i]:
             ++vowelCount
@@ -24,5 +26,8 @@ def countVowels(thisWord,characterCount,vowels,vowelCount):
         else:
             ++consonantCount
     return vowelCount,consonantCount          
-            
+
+def makeCharArray(thisWord):
+    return [char for char in thisWord]
+
 main()
